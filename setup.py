357e123a8e448c3df19adfc01557effe2cfeb668
Julien Bigot <julien.bigot@cea.fr>
2022-01-23 22:35:04 +0100

    Fix an issue when installing through wheels

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@
 from os import listdir, makedirs
 from os.path import abspath, dirname, expanduser, isdir, join, relpath, samefile
 from platform import system
+from platform import system
+from re import match
 from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
@@ -36,43 +38,46 @@ try:
 except ImportError:
     from __builtin__ import bytes
 
+
 def install_helpers_method(self):
     print("running install_wrappers")
-    
+
     share_dir = join('share', self.config_vars["dist_name"])
-    cmake_dirs = [ join(share_dir, 'cmake'), join('share', 'bpp', 'cmake') ]
-    
+    cmake_dirs = [join(share_dir, 'cmake'), join('share', 'bpp', 'cmake')]
+
     prefix = self.install_data
-    
+
     if self.user:
         print("Registering in cmake user registry")
         if system() == 'Linux':
             cmake_registry = abspath(expanduser("~/.cmake/packages/"))
             if (self.root is not None):
-              cmake_registry = abspath(abspath(self.root)+cmake_registry)
+                cmake_registry = abspath(abspath(self.root)+cmake_registry)
             if not isdir(cmake_registry):
                 makedirs(cmake_registry)
-            cmake_registry_file = join(cmake_registry, self.config_vars["dist_fullname"])
+            cmake_registry_file = join(
+                cmake_registry, self.config_vars["dist_fullname"])
             print("Writing cmake registry file: "+cmake_registry_file)
             with open(cmake_registry_file, 'w') as zppfile:
                 zppfile.write(join(self.install_userbase, cmake_dirs[0])+"\n")
         else:
-            print("Not registering in cmake user registry: unsupported system type ("+system()+")")
+            print(
+                "Not registering in cmake user registry: unsupported system type ("+system()+")")
     else:
         if isdir(prefix) and samefile(prefix, '/'):
             prefix = join(prefix, 'usr')
-    
+
     share_dir = join(prefix, share_dir)
-    cmake_dirs = map( lambda d: join(prefix, d), cmake_dirs)
-    
+    cmake_dirs = map(lambda d: join(prefix, d), cmake_dirs)
+
     if not isdir(share_dir):
         makedirs(share_dir)
     for res_name in listdir('zpp'):
-        if ( res_name[-3:] == '.mk' ):
+        if (res_name[-3:] == '.mk'):
             dst = join(share_dir, res_name)
             print('Installing '+res_name+' wrapper to '+share_dir)
             copyfileobj(open(join('zpp', res_name), 'rb'), open(dst, 'wb'))
-    
+
     for cmake_dir in cmake_dirs:
         if not isdir(cmake_dir):
             makedirs(cmake_dir)
@@ -87,49 +92,61 @@ def install_helpers_method(self):
             data_in = open(join('zpp', 'cmake', res_name), 'rb')
             with open(dst, 'wb') as data_out:
                 for line in data_in:
-                    if bytes(b'@PYTHON_INSERT_ZPP_EXECUTABLE@') in line:
-                        data_out.write(bytes(b'get_filename_component(ZPP_EXECUTABLE "${_CURRENT_LIST_DIR}/'+rel_cmake_path+b'/zpp" ABSOLUTE)\n'))
+                    if (bytes(b'@PYTHON_INSERT_ZPP_EXECUTABLE@') in line) and (
+                        not match(r'.*/bdist\.[^/]*/wheel/zpp-[^/]*\.data/scripts',
+                                  self.install_scripts)):
+                        data_out.write(bytes(
+                            b'get_filename_component(ZPP_EXECUTABLE "${_CURRENT_LIST_DIR}/' +
+                            rel_cmake_path + b'/zpp" ABSOLUTE)\n'))
                     else:
                         data_out.write(line)
-    
+
 
 class PostDevelopCommand(develop):
     install_helpers = install_helpers_method
+
     def run(self):
         develop.run(self)
         self.install_helpers()
 
+
 class PostInstallCommand(install):
     install_helpers = install_helpers_method
+
     def run(self):
         install.run(self)
         self.install_helpers()
+
 
 version = {}
 with open("zpp/version.py") as fp:
     exec(fp.read(), version)
 
 setup(
-    packages = [ 'zpp' ],
-    zip_safe = True,
-    entry_points = { "console_scripts": [ "zpp = zpp:main", "bpp = zpp:main" ] },
-    package_data = { "zpp": ["include/*.zpp.sh"] },
-    install_requires = [ 'setuptools' ],
-    cmdclass = { 'develop': PostDevelopCommand, 'install': PostInstallCommand },
-    
-    name = "zpp",
-    version = version['__version__'],
-    author = "Julien Bigot",
-    author_email = "julien.bigot@cea.fr",
-    description = "a Bash Pre-Processor for Fortran. ZPP is useful in order to build clean Fortran90 interfaces. It allows to generate Fortran code for all types, kinds, and array ranks supported by the compiler.",
-    long_description = open(join(dirname(__file__), 'README.md'), 'r').read(),
-    long_description_content_type = "text/markdown",
-    license = "MIT",
-    keywords = "bash Fortran pre-processor",
-    project_urls = {
+    packages=['zpp'],
+    zip_safe=True,
+    entry_points={"console_scripts": ["zpp = zpp:main", "bpp = zpp:main"]},
+    package_data={"zpp": ["include/*.zpp.sh"]},
+    install_requires=['setuptools'],
+    cmdclass={'develop': PostDevelopCommand, 'install': PostInstallCommand},
+
+    name="zpp",
+    version=version['__version__'],
+    url='https://github.com/pdidev/zpp/',
+    author="Julien Bigot",
+    author_email="julien.bigot@cea.fr",
+    description="a Bash Pre-Processor for Fortran. " +
+                "ZPP is useful in order to build clean Fortran90 interfaces. " +
+                "It allows to generate Fortran code for all types, kinds, and array ranks " +
+                "supported by the compiler.",
+    long_description=open(join(dirname(__file__), 'README.md'), 'r').read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="bash Fortran pre-processor",
+    project_urls={
         "Source Code": "https://github.com/pdidev/zpp/",
     },
-    classifiers = [
+    classifiers=[
         "License :: OSI Approved :: MIT License",
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
